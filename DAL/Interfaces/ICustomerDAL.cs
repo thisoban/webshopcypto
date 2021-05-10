d685d8123f3a0ae9ca67aa8870f5ab0f8dc0afcb
Thisoban Mahalingam <thisoban_1995@hotmail.com>
2021-05-10 20:27:23 +0200

    tweaked coding interfaces removed repository

---
+++ DAL/Interfaces/ICustomerDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+
+namespace DAL.Interfaces
+{
+    public interface ICustomerDAL
+    {
+        //getcustomer
+        Customer GetCustomer(int id);
+        //instertcustomer
+        bool InsertCustomer(Customer customer);
+        //updatecustomer
+        bool UpdateCustomer(Customer customer);
+    }
+}
---
+++ DAL/Interfaces/IUserDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+
+namespace DAL.Interfaces
+{
+    public interface IUserDAL
+    {
+        bool CreateUser(User user);
+        User GetUser(User user);
+        bool UpdateUser(User user);
+    }
+}
