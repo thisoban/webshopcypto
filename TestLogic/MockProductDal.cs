b1db1a0d127d611feb6dbea5a01b3527d6aacb0c
Thisoban Mahalingam <I419727@fhict.nl>
2021-06-11 17:26:35 +0200

    tweaking mockproductdal for unit testing

--- TestLogic/MockProductDal.cs
+++ TestLogic/MockProductDal.cs
@@ -11,16 +11,8 @@ namespace TestLogic
 {
     public class MockProductDal : IProductDal
     {
-        List<Product> products = new List<Product>();
-        public void CreateProduct(Product productmodel)
-        {
-                products.Add(productmodel);
-        }
-
-        public List<Product> GetAllproducts()
-        {
-            List<Product> products = new List<Product>();
-            Product product1 = new Product() {
+        List<Product> products = new List<Product>() {
+              new Product() {
                 Id = 1,
                 Description ="nice coin to have",
                 Name = "bitcoin",
@@ -28,37 +20,41 @@ namespace TestLogic
                 Buyprice = 5000,
                 Serialnumber = 123,
                 Quantity = 3
-            };
-            Product product2 = new Product()
-            {
-                Id = 1,
-                Description = "nice coin to have bought",
-                Name = "litecoin",
-                SellPrice = 13000,
-                Buyprice = 3000,
-                Serialnumber = 456,
-                Quantity = 5
-            };
-            Product product3 = new Product()
-            {
-                Id = 1,
-                Description = "coin that it sought after",
-                Name = "thetacoin",
-                SellPrice = 13000,
-                Buyprice = 8000,
-                Serialnumber = 789,
-                Quantity = 5
-            };
-            products.Add(product1);
-            products.Add(product2);
-            products.Add(product3);
+            },
+        new Product()
+        {
+            Id = 2,
+            Description = "nice coin to have bought",
+            Name = "litecoin",
+            SellPrice = 13000,
+            Buyprice = 3000,
+            Serialnumber = 456,
+            Quantity = 5
+         },
+        new Product()
+        {
+            Id = 3,
+            Description = "coin that it sought after",
+            Name = "thetacoin",
+            SellPrice = 13000,
+            Buyprice = 8000,
+            Serialnumber = 789,
+            Quantity = 5
+        }
+       };
+    
+        public void CreateProduct(Product productmodel)
+        {
+                products.Add(productmodel);
+        }
+
+        public List<Product> GetAllproducts()
+        {
             return products;
-           
         }
 
         public Product GetProduct(int id)
         {
-
             Product product = new Product
             {
                 Name = "tester",
@@ -75,14 +71,9 @@ namespace TestLogic
         {
             throw new NotImplementedException();
         }
-
         public void UpdateProduct(Product productmodel)
         {
-            if (productmodel.Id == 5)
-            {
-                
-            }
-            
+            products.Where(x => x.Id == productmodel.Id);
         }
     }
 }
